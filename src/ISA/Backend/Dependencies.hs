f1db088fbad1f2fe0b1f903ec3461286d37c2c64
Georgy Lukyanov <mail@geo2a.info>
2019-11-09 18:24:41 +0000

    Implement data-dependency graph semantics

--- src/ISA/Backend/Dependencies.hs
+++ src/ISA/Backend/Dependencies.hs
@@ -11,13 +11,22 @@
 -----------------------------------------------------------------------------
 
 module ISA.Backend.Dependencies
-  (dependencies) where
+  (dependencies
+  , programDataGraph
+  , drawGraph) where
 
+import           Algebra.Graph            hiding (graph)
+import           Algebra.Graph.Export.Dot
+import           Control.Arrow            (first, second)
 import           Control.Selective
-import           Data.Either       (partitionEithers)
+import           Data.Either              (partitionEithers)
+import           Data.Maybe               (fromJust)
+import qualified Data.Set                 as Set
 
 import           FS
+import           ISA.Semantics
 import           ISA.Types
+import           ISA.Types.Instruction
 
 trackingRead :: key -> Over [Either key key] a
 trackingRead key = Over [Left key]
@@ -31,3 +40,45 @@ dependencies :: Value a => FS key Selective Value a
 dependencies task =
     partitionEithers . getOver $
     task trackingRead trackingWrite
+
+type KeyLabel = String
+
+type InstructionLabel = String
+
+-- | Compute static data flow graph of an instruction. In case of supplying a
+--   monadic, i.e. data-dependent instruction, 'Nothing' is returned.
+--
+-- Since no data requiring simulation is performed, the semantics metalanguage
+-- terms are mocked: 'read' becomes 'const 0' and 'write' is simply ignored.
+instructionGraph :: Value a => (Address, Instruction a)
+                            -> Maybe (Graph (Either Key InstructionLabel))
+instructionGraph i@(addr, instr) = do
+    let (ins, outs) = dependencies (instructionSemantics instr)
+    let instrInfo = instructionLabel
+    pure $ overlay (star (Right instrInfo) (map Left outs))
+                   (transpose $ star (Right instrInfo) (map Left ins))
+    where instructionLabel = (show addr <> "|" <> show instr)
+
+-- | Serialise data flow graph as a .dot string
+drawGraph :: Graph (Either Key InstructionLabel) -> String
+drawGraph g = export style g
+  where
+    style = defaultStyleViaShow
+        { vertexName = \v -> "v" ++ show (fromJust $ Set.lookupIndex v names)
+        , vertexAttributes = \x -> case x of
+            Left  k      -> [ "shape"  := "circle"
+                            , "label"  := show k ]
+            Right i -> [ "shape" := "record"
+                            , "label" := i ] }
+    names = vertexSet g
+    -- instructionLabel a i = fromString (show a <> "|" <> show i)
+
+-- | Compute static data flow graph of a program. In case of supplying a
+--   monadic, i.e. data-dependent instruction, 'Nothing' is returned.
+programDataGraph :: Value a => [(Address, Instruction a)]
+                            -> Maybe (Graph (Either Key InstructionLabel))
+programDataGraph p =
+    let p' = map (second id) p
+    in  foldl go (Just empty) (map instructionGraph p')
+    where go _   Nothing = Nothing
+          go acc g       = overlay <$> acc <*> g
