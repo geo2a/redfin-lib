09fc4f114b85325a714044da5c5f8d17f3766301
Georgy Lukyanov <mail@geo2a.info>
2020-08-25 13:54:20 +0200

    Drop debugger

--- src/ISA/Backend/Symbolic/List/Run.hs
+++ src/ISA/Backend/Symbolic/List/Run.hs
@@ -13,21 +13,17 @@
 --
 -----------------------------------------------------------------------------
 
-module ISA.Backend.Symbolic.List.Run
-    (debugConsole, runModel) where
+module ISA.Backend.Symbolic.List.Run ( runModel
+                                     ) where
 
-import           Colog.Core                   (LogAction (..), logStringStdout,
-                                               (<&))
-import           Control.Monad                (ap, join, liftM)
-import           Control.Monad.IO.Class       (MonadIO, liftIO)
-import           Control.Monad.Reader.Class
+import           Control.Monad.IO.Class       ()
+import           Control.Monad.Reader.Class   ()
 import           Control.Monad.State          (evalState)
 import           Control.Monad.State.Class
 import           Data.Functor                 (void)
-import           Data.IORef
+import           Data.IORef                   ()
 import qualified Data.Map.Strict              as Map
 import           Prelude                      hiding (log)
-import           System.Console.Haskeline
 
 import           ISA.Backend.Symbolic.List
 import           ISA.Semantics
@@ -100,79 +96,3 @@ runModelM steps s = do
 
 runModel :: Int -> Context -> Trace Context
 runModel steps s = evalState (runModelM steps s) 0
-
-data DebugEnv = MkDebugEnv
-  { _log    :: LogAction IO String
-  , _ui     :: Context -> IO ()
-  , _stepId :: IORef NodeId
-  -- , _stepsLeft  :: IORef Int
-  }
-
-newtype Debugger a = MkDebugger {runDebugger :: DebugEnv -> IO a}
-  deriving Functor
-
-instance Applicative Debugger where
-  pure = return
-  (<*>) = ap
-  (*>) = (>>)
-
-instance Monad Debugger where
-  return x = MkDebugger $ \_ -> return x
-  x >>= f = MkDebugger $ \env -> do
-    y <- runDebugger x env
-    runDebugger (f y) env
-
-instance (MonadReader DebugEnv) Debugger where
-  ask = MkDebugger $ \env -> pure env
-  local = error "Debugger.MonadReader.local is not implemented"
-
-instance (MonadState NodeId) Debugger where
-  get = MkDebugger $ \env ->
-    readIORef (_stepId env)
-  put s = MkDebugger $ \env ->
-    writeIORef (_stepId env) s
-  -- modify f = MkDebugger $ \env ->
-  --   modifyIoRef (_stepId env) s
-
-instance MonadIO Debugger where
-  liftIO f = MkDebugger $ \env -> f
-
-instance MonadException Debugger where
-  controlIO f = MkDebugger $ \env -> controlIO $ \(RunIO run) -> let
-                  run' = RunIO (fmap (MkDebugger . const) . run . flip runDebugger env)
-                  in fmap (flip runDebugger env) $ f run'
-
-debugImpl :: Int -> Context -> InputT Debugger (Trace Context)
-debugImpl steps ctx = do
-  -- log <- _log <$> ask
-  -- ui  <- _ui <$> ask
-  -- liftIO $ ui ctx
-  -- modify (+ 1)
-  -- n <- get
-  let n = -1
-  outputStrLn "Current state: "
-  outputStrLn $ show ctx
-  minput <- getInputLine "% "
-  case minput of
-      Nothing -> pure (mkTrace (Node n ctx) [])
-      Just "quit" -> pure (mkTrace (Node n ctx) [])
-      Just input -> do
-        outputStrLn $ "Input was: " ++ input
-        let halted = case Map.lookup (F Halted) (_bindings ctx) of
-              Just b  -> b
-              Nothing -> error "Engine.runModel: uninitialised flag Halted!"
-        let newStates = step ctx
-        if | steps <= 0 -> pure (mkTrace (Node n ctx) [])
-           | otherwise  ->
-             case halted of
-               SConst (CBool True) -> pure (mkTrace (Node n ctx) [])
-               _ -> do
-                 children <- traverse (debugImpl (steps - 1)) newStates
-                 pure $ mkTrace (Node n ctx) children
-
-debugConsole :: Int -> Context -> IO ()
-debugConsole steps ctx = do
-  nodeRef <- newIORef 0
-  let env = MkDebugEnv undefined undefined nodeRef
-  runDebugger (runInputT defaultSettings (debugImpl steps ctx)) undefined
-  putStrLn "Debug session ended."
