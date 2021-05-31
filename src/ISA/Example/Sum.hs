7fe1291c3d6e7f282d8d55f44ccd1e8d2d3a7bc5
Georgy Lukyanov <mail@geo2a.info>
2021-05-31 16:43:23 +0200

    Try to specify that result is the sum

--- src/ISA/Example/Sum.hs
+++ src/ISA/Example/Sum.hs
@@ -14,8 +14,12 @@ module ISA.Example.Sum (
     demo,
     sumArrayLowLevel,
     initCtx,
+    symExec,
+    proving,
 ) where
 
+import Criterion.Main
+import Data.Int (Int32)
 import qualified Data.Map as Map
 import qualified Data.Text as Text
 import Prelude hiding (not)
@@ -23,6 +27,7 @@ import Prelude hiding (not)
 import ISA.Assembly
 import ISA.Backend.Graph
 import ISA.Backend.Graph.BasicBlock
+import ISA.Backend.Symbolic.Zipper
 import ISA.Backend.Symbolic.Zipper.Run
 import ISA.Types
 import ISA.Types.Boolean
@@ -37,7 +42,7 @@ sumArrayLowLevel :: Script
 sumArrayLowLevel = do
     let pointer = 0; sum = 253; array_start = 255 -- ; pointer_store
     let r0 = R0; r1 = R1; r2 = R2
-    -- ld_i r0 0
+    -- ld_i r0 42
     -- st r0 sum
     ld r1 pointer
     -- add_i r1 1
@@ -60,9 +65,9 @@ sumArrayLowLevel = do
     "end" @@ ld r0 sum
     halt
 
-array :: CAddress -> Int -> (Sym -> Sym) -> ([(Key, Sym)], [(Text.Text, Sym)])
+array :: CAddress -> Int32 -> (Sym -> Sym) -> ([(Key, Sym)], [(Text.Text, Sym)])
 array start size varConstraint =
-    let addrs = [start .. start + fromIntegral size]
+    let addrs = [start .. start + fromIntegral size - 1]
         vars = map SAny . map ("x" <>) . map (Text.pack . show) $ addrs
         constraints = map varConstraint vars
      in ( zip (map (Addr . literal) addrs) vars
@@ -70,17 +75,22 @@ array start size varConstraint =
         )
 
 initCtx :: Context Sym
-initCtx =
-    let (vars, constrs) = array 1 n (\x -> (SGt x 0) &&& (SLt x 10000))
-        n = 10
+initCtx = mkInitCtx 3 0 1000
+
+mkInitCtx :: Int32 -> Int32 -> Int32 -> Context Sym
+mkInitCtx n xMin xMax =
+    let (vars, constrs) =
+            array 1 n (\x -> (SGt x (SConst (CInt32 xMin)) &&& (SLt x (SConst (CInt32 xMax)))))
      in MkContext
             { _pathCondition = true
             , _store = Map.empty
             , _constraints =
                 constrs
-                    ++ [ ("n", (SGt (SAny "n") (0 -1)) &&& (SLt (SAny "n") (SConst (CInt32 $ fromIntegral n))))
-                    -- , ("n", (SLt (SAny "n") 4))
-                    -- , ("n", (SLt (SAny "n") 4))
+                    ++ [
+                           ( "n"
+                           , (SGt (SAny "n") (0 -1))
+                                &&& (SLt (SAny "n") (SConst (CInt32 $ fromIntegral n + 1)))
+                           )
                        ]
             , _bindings =
                 Map.fromList $
@@ -106,13 +116,39 @@ all_finally_halted = either undefined id (parseTheorem "" "F ([Halted])")
 
 all_globally_no_overflow = either undefined id (parseTheorem "" "G (![Overflow])")
 
------ Demo
+sum_is_correct n =
+    ACTLAllF $ statePredicate n
+  where
+    statePredicate n =
+        let inputVars = map SAny . map (Text.pack . ("x" <>) . show) $ [1 .. n]
+            fixedN =
+                ASym (SAny "n" `SEq` SConst (CInt32 n))
+            resultIsTheSum =
+                AEq (AKey (Reg R0)) (ASym (sum inputVars))
+         in (fixedN `aImplies` resultIsTheSum)
+
+-- either undefined id $
+--     parseTheorem "" "F ([0] == 1)"
+
+symExec :: Int32 -> IO Trace
+symExec n = runModel 1000 (mkInitCtx n 0 1000)
+
+proving :: Trace -> IO ()
+proving trace = do
+    print =<< prove trace all_finally_halted
+    print =<< prove trace all_globally_no_overflow
+
+----- Demos
 demo :: IO ()
 demo = do
-    trace <- runModel 100 initCtx
+    let n = 2
+    trace <- runModel 10000 (mkInitCtx n 0 1000)
+    -- print $ evalACTL trace (negateACTL (sum_is_correct n))
     r1 <- prove trace all_finally_halted
-    print r1
-    -- print (solution r1)
     r2 <- prove trace all_globally_no_overflow
-    print r2
+    r3 <- prove trace (sum_is_correct n)
+    -- print (r1, r2, r3)
+    print r3
     pure ()
+
+-- not ($n == 0) || (42 == 0)
